b7ce9a72c84560074e0bc90341ffafa22c6ee788
vivekforbes <vivekanandhan87@gmail.com>
2014-04-15 20:34:19 -0400

    Fixed memory leak issue in character selection screen

--- TeamJones_Project/src/mainpackage/Screens/CharacterSelectScreen.java
+++ TeamJones_Project/src/mainpackage/Screens/CharacterSelectScreen.java
@@ -44,7 +44,7 @@ public class CharacterSelectScreen implements Screen {
 	private TextureAtlas atlas;
 	private Skin skin;
 	private TextButton btnPlayGame;
-	
+
 	// music
 	private Music charSelectionMusic;
 	private SoundFiles soundFiles;
@@ -52,8 +52,9 @@ public class CharacterSelectScreen implements Screen {
 	// character selection
 	private Texture p1Texture;
 	private Texture p2Texture;
-	private Image imgP1;
-	private Image imgP2;
+	private Texture texIchigo, texByakuya, texIkkaku, texIshida, texKenpachi,
+			texUrahara, texkomamura, texMayuri, texRenji, texP1, texP2,
+			texP1Ready, texP2Ready;
 	private Image chP1;
 	private Image chP2;
 	private Image chP1Ready;
@@ -65,7 +66,7 @@ public class CharacterSelectScreen implements Screen {
 
 	// highlight the character
 	private ShapeRenderer sRenderer;
-	
+
 	// Holds the screens volume levels when loaded in
 	private float bgmVolume;
 	private float sfxVolume;
@@ -73,8 +74,6 @@ public class CharacterSelectScreen implements Screen {
 	// constructor to keep a reference to the main Game class
 	public CharacterSelectScreen(Game game) {
 		this.game = game;
-		
-		
 	}
 
 	// called when the screen should render itself
@@ -82,8 +81,65 @@ public class CharacterSelectScreen implements Screen {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 
-		stage.act(delta);
+		// stage.act(delta);
 		// draw objects into the screen
+		batch.begin();
+
+		batch.draw(splashTexture, 0, 0, 800, 600, 0, 0,
+				splashTexture.getWidth(), splashTexture.getHeight(), false,
+				false);
+
+		batch.draw(p1Texture, 10, 50, 300, 550, 0, 0, p1Texture.getWidth(),
+				p1Texture.getHeight(), false, false);
+
+		batch.draw(p2Texture, 480, 50, 300, 550, 0, 0, p2Texture.getWidth(),
+				p2Texture.getHeight(), false, false);
+
+		batch.draw(texIchigo, 20, 50, 70, 120, 0, 0, texIchigo.getWidth(),
+				texIchigo.getHeight(), false, false);
+
+		batch.draw(texByakuya, 105, 50, 70, 120, 0, 0, texByakuya.getWidth(),
+				texByakuya.getHeight(), false, false);
+
+		batch.draw(texIkkaku, 190, 50, 70, 120, 0, 0, texIkkaku.getWidth(),
+				texIkkaku.getHeight(), false, false);
+
+		batch.draw(texIshida, 275, 50, 70, 120, 0, 0, texIshida.getWidth(),
+				texIshida.getHeight(), false, false);
+
+		batch.draw(texKenpachi, 360, 50, 70, 120, 0, 0, texKenpachi.getWidth(),
+				texKenpachi.getHeight(), false, false);
+
+		batch.draw(texUrahara, 445, 50, 70, 120, 0, 0, texUrahara.getWidth(),
+				texUrahara.getHeight(), false, false);
+
+		batch.draw(texkomamura, 530, 50, 70, 120, 0, 0, texkomamura.getWidth(),
+				texkomamura.getHeight(), false, false);
+
+		batch.draw(texMayuri, 615, 50, 70, 120, 0, 0, texMayuri.getWidth(),
+				texMayuri.getHeight(), false, false);
+
+		batch.draw(texRenji, 700, 50, 70, 120, 0, 0, texRenji.getWidth(),
+				texRenji.getHeight(), false, false);
+
+		/*
+		 * batch.draw(texP1, 0, 170, 100, 50, 0, 0, texP1.getWidth(),
+		 * texP1.getHeight(), false, false);
+		 * 
+		 * batch.draw(texP2, 680, 0, 100, 50, 0, 0, texP2.getWidth(),
+		 * texP2.getHeight(), false, false);
+		 */
+
+		/*
+		 * batch.draw(texP1Ready, 10, 50, 300, 550, 0, 0, texP1Ready.getWidth(),
+		 * texP1Ready.getHeight(), false, false);
+		 * 
+		 * batch.draw(texP2Ready, 480, 50, 300, 550, 0, 0,
+		 * texP2Ready.getWidth(), texP2Ready.getHeight(), false, false);
+		 */
+
+		// stage.draw();
+		batch.end();
 		batch.begin();
 		stage.draw();
 		batch.end();
@@ -94,7 +150,7 @@ public class CharacterSelectScreen implements Screen {
 
 	// called when the screen resized
 	public void resize(int width, int height) {
-		
+
 		if (stage == null) {
 			stage = new Stage(width, height, true);
 		}
@@ -107,12 +163,6 @@ public class CharacterSelectScreen implements Screen {
 
 		// set the background image to the menu screen
 		splashTexture = TextureFiles.getBackgroundTexture("characterSelection");
-		splashTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		Image img = new Image(new TextureRegion(splashTexture));
-		// fit the full image to the screen
-		img.setFillParent(true);
-		// adds an actor to the root of the stage.
-		stage.addActor(img);
 
 		// creates the play game button
 		TextButtonStyle txtPlayStyle = new TextButtonStyle();
@@ -128,122 +178,43 @@ public class CharacterSelectScreen implements Screen {
 		btnPlayGame.setDisabled(true);
 		stage.addActor(btnPlayGame);
 
-		btnPlayGame.addListener(new InputListener() {
-
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y,
-					int pointer, int button) {
-				return true;
-			}
-
-			@Override
-			public void touchUp(InputEvent event, float x, float y,
-					int pointer, int button) {
-				soundFiles = new SoundFiles();
-				soundFiles.playSound("menuSelect", sfxVolume);
-				// hides/disables current screen
-				hide();
-				// go to Game Screen
-				game.setScreen(new GameScreen(game));
-			}
-		});
-
 		// texture for player1
 		// default character is ichigo
 		p1Texture = TextureFiles.getCharacterTexture("ichigo");
-		p1Texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		imgP1 = new Image(new TextureRegion(p1Texture));
-		// adds an actor to the root of the stage.
-		imgP1.setBounds(10, 50, 300, 550);
-		stage.addActor(imgP1);
 
 		// texture for player2
-		// default character is renji
-		p2Texture = TextureFiles.getCharacterTexture("renji");
-		p2Texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		imgP2 = new Image(new TextureRegion(p2Texture));
-		// adds an actor to the root of the stage.
-		imgP2.setBounds(480, 50, 300, 550);
-		stage.addActor(imgP2);
+		// default character is byakuya
+		p2Texture = TextureFiles.getCharacterTexture("byakuya");
 
 		// ichigo texture
-		Texture texIchigo = TextureFiles.getCharacterSelectionTexture("ichigo");
-		texIchigo.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		Image imgIchigo = new Image(new TextureRegion(texIchigo));
-		// adds an actor to the root of the stage.
-		imgIchigo.setBounds(20, 50, 70, 120);
-		stage.addActor(imgIchigo);
+		texIchigo = TextureFiles.getCharacterSelectionTexture("ichigo");
 
 		// byakuya texture
-		Texture texByakuya = TextureFiles
-				.getCharacterSelectionTexture("byakuya");
-		texByakuya.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		Image imgByakuya = new Image(new TextureRegion(texByakuya));
-		// adds an actor to the root of the stage.
-		imgByakuya.setBounds(105, 50, 70, 120);
-		stage.addActor(imgByakuya);
+		texByakuya = TextureFiles.getCharacterSelectionTexture("byakuya");
 
 		// ikkaku texture
-		Texture texIkkaku = TextureFiles.getCharacterSelectionTexture("ikkaku");
-		texIkkaku.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		Image imgIkkaku = new Image(new TextureRegion(texIkkaku));
-		// adds an actor to the root of the stage.
-		imgIkkaku.setBounds(190, 50, 70, 120);
-		stage.addActor(imgIkkaku);
+		texIkkaku = TextureFiles.getCharacterSelectionTexture("ikkaku");
 
 		// ishida texture
-		Texture texIshida = TextureFiles.getCharacterSelectionTexture("ishida");
-		texIshida.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		Image imgIshida = new Image(new TextureRegion(texIshida));
-		// adds an actor to the root of the stage.
-		imgIshida.setBounds(275, 50, 70, 120);
-		stage.addActor(imgIshida);
+		texIshida = TextureFiles.getCharacterSelectionTexture("ishida");
 
 		// kenpachi texture
-		Texture texKenpachi = TextureFiles
-				.getCharacterSelectionTexture("kenpachi");
-		texKenpachi.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		Image imgKenpachi = new Image(new TextureRegion(texKenpachi));
-		// adds an actor to the root of the stage.
-		imgKenpachi.setBounds(360, 50, 70, 120);
-		stage.addActor(imgKenpachi);
+		texKenpachi = TextureFiles.getCharacterSelectionTexture("kenpachi");
 
 		// urahara texture
-		Texture texUrahara = TextureFiles
-				.getCharacterSelectionTexture("urahara");
-		texUrahara.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		Image imgUrahara = new Image(new TextureRegion(texUrahara));
-		// adds an actor to the root of the stage.
-		imgUrahara.setBounds(445, 50, 70, 120);
-		stage.addActor(imgUrahara);
+		texUrahara = TextureFiles.getCharacterSelectionTexture("urahara");
 
 		// komamura texture
-		Texture texkomamura = TextureFiles
-				.getCharacterSelectionTexture("komamura");
-		texkomamura.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		Image imgKomamura = new Image(new TextureRegion(texkomamura));
-		// adds an actor to the root of the stage.
-		imgKomamura.setBounds(530, 50, 70, 120);
-		stage.addActor(imgKomamura);
+		texkomamura = TextureFiles.getCharacterSelectionTexture("komamura");
 
 		// mayuri texture
-		Texture texMayuri = TextureFiles.getCharacterSelectionTexture("mayuri");
-		texMayuri.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		Image imgMayuri = new Image(new TextureRegion(texMayuri));
-		// adds an actor to the root of the stage.
-		imgMayuri.setBounds(615, 50, 70, 120);
-		stage.addActor(imgMayuri);
+		texMayuri = TextureFiles.getCharacterSelectionTexture("mayuri");
 
 		// renji texture
-		Texture texRenji = TextureFiles.getCharacterSelectionTexture("renji");
-		texRenji.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		Image imgRenji = new Image(new TextureRegion(texRenji));
-		// adds an actor to the root of the stage.
-		imgRenji.setBounds(700, 50, 70, 120);
-		stage.addActor(imgRenji);
+		texRenji = TextureFiles.getCharacterSelectionTexture("renji");
 
 		// player 1 selection texture
-		Texture texP1 = TextureFiles.getUtilityTexture("player1");
+		texP1 = TextureFiles.getUtilityTexture("player1");
 		texP1.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		chP1 = new Image(new TextureRegion(texP1));
 		// adds an actor to the root of the stage.
@@ -251,15 +222,15 @@ public class CharacterSelectScreen implements Screen {
 		stage.addActor(chP1);
 
 		// player 2 selection texture
-		Texture texP2 = TextureFiles.getUtilityTexture("player2");
+		texP2 = TextureFiles.getUtilityTexture("player2");
 		texP2.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		chP2 = new Image(new TextureRegion(texP2));
 		// adds an actor to the root of the stage.
-		chP2.setBounds(680, 0, 100, 50);
+		chP2.setBounds(85, 0, 100, 50);
 		stage.addActor(chP2);
 
 		// player 1 ready texture
-		Texture texP1Ready = TextureFiles.getUtilityTexture("ready");
+		texP1Ready = TextureFiles.getUtilityTexture("ready");
 		texP1Ready.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		chP1Ready = new Image(new TextureRegion(texP1Ready));
 		// adds an actor to the root of the stage.
@@ -268,34 +239,13 @@ public class CharacterSelectScreen implements Screen {
 		stage.addActor(chP1Ready);
 
 		// player 2 ready texture
-		Texture texP2Ready = TextureFiles.getUtilityTexture("ready");
+		texP2Ready = TextureFiles.getUtilityTexture("ready");
 		texP2Ready.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		chP2Ready = new Image(new TextureRegion(texP2Ready));
 		// adds an actor to the root of the stage.
 		chP2Ready.setBounds(480, 50, 300, 550);
 		chP2Ready.setVisible(false);
 		stage.addActor(chP2Ready);
-
-		btnPlayGame.addListener(new InputListener() {
-
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y,
-					int pointer, int button) {
-				return true;
-			}
-
-			@Override
-			public void touchUp(InputEvent event, float x, float y,
-					int pointer, int button) {
-				// this is where functionality for next screen should be called
-				// play menu select sound
-				soundFiles = new SoundFiles();
-				soundFiles.playSound("menuSelect", sfxVolume);
-				// go to Game Screen
-				charSelectionMusic.stop();
-				game.setScreen(new GameScreen(game));
-			}
-		});
 	}
 
 	// method to draw highlighted rectangle
@@ -312,7 +262,7 @@ public class CharacterSelectScreen implements Screen {
 		// Set volume levels upon loading
 		bgmVolume = UserConfig.getBGMVolume(false);
 		sfxVolume = UserConfig.getSFXVolume(false);
-		
+
 		batch = new SpriteBatch();
 		atlas = new TextureAtlas("assets/gui/button.pack");
 		skin = new Skin();
@@ -330,7 +280,7 @@ public class CharacterSelectScreen implements Screen {
 
 	// called when current screen changes from this to a different screen
 	public void hide() {
-		
+
 	}
 
 	// called when game paused
@@ -354,14 +304,14 @@ public class CharacterSelectScreen implements Screen {
 
 	// method to return the key selection
 	public void keyDown(int keycode) {
-		soundFiles = new SoundFiles();		
-		switch(keycode)
-		{
-		case Keys.A: 
-		{
+		soundFiles = new SoundFiles();
+
+		switch (keycode) {
+		case Keys.A: {
 			// return if prior than character 1
 			if (ch1 <= 0 || chP1Ready.isVisible())
 				break;
+			p1Texture.dispose();
 			// change index based on character selection
 			ch1Index--;
 			// value that change the texture position
@@ -369,15 +319,16 @@ public class CharacterSelectScreen implements Screen {
 			// set new texture position
 			chP1.setPosition(ch1, 170);
 			// change the image texture
-			imgP1.setDrawable(new SpriteDrawable(changeCharacter(ch1Index)));
-			// play menu select sound			
+			p1Texture = changeCharacter(ch1Index);
+			// play menu select sound
 			soundFiles.playSound("menuTraverse", sfxVolume);
 			break;
 		}
-		case Keys.D:  {
+		case Keys.D: {
 			// return if further than last character
 			if (ch1 >= 680 || chP1Ready.isVisible())
 				break;
+			p1Texture.dispose();
 			// change index based on character selection
 			ch1Index++;
 			// value that change the texture position
@@ -385,28 +336,30 @@ public class CharacterSelectScreen implements Screen {
 			// set new texture position
 			chP1.setPosition(ch1, 170);
 			// change the image texture
-			imgP1.setDrawable(new SpriteDrawable(changeCharacter(ch1Index)));
+			p1Texture = changeCharacter(ch1Index);
 			// play menu select sound
 			soundFiles.playSound("menuTraverse", sfxVolume);
 			break;
 		}
-		case Keys.RIGHT:  {
+		case Keys.RIGHT: {
 			if (ch2 >= 680 || chP2Ready.isVisible())
 				break;
+			p2Texture.dispose();
 			ch2Index++;
 			// value that change the texture position
 			ch2 += 85;
 			// set new texture position
 			chP2.setPosition(ch2, 0);
 			// change the image texture
-			imgP2.setDrawable(new SpriteDrawable(changeCharacter(ch2Index)));
+			p2Texture = changeCharacter(ch2Index);
 			// play menu select sound
 			soundFiles.playSound("menuTraverse", sfxVolume);
 			break;
 		}
-		case Keys.LEFT:  {
+		case Keys.LEFT: {
 			if (ch2 <= 0 || chP2Ready.isVisible())
 				break;
+			p2Texture.dispose();
 			// change index based on character selection
 			ch2Index--;
 			// value that change the texture position
@@ -414,47 +367,41 @@ public class CharacterSelectScreen implements Screen {
 			// set new texture position
 			chP2.setPosition(ch2, 0);
 			// change the image texture
-			imgP2.setDrawable(new SpriteDrawable(changeCharacter(ch2Index)));
+			p2Texture = changeCharacter(ch2Index);
 			// play menu select sound
 			soundFiles.playSound("menuTraverse", sfxVolume);
 			break;
 		}
 
-		case Keys.SPACE:  {
-			if (chP1Ready.isVisible() && chP2Ready.isVisible()) {
-				btnPlayGame.setText("Ready");
-				btnPlayGame.setDisabled(false);
-			}
+		case Keys.SPACE: {			
 			chP1Ready.setVisible(true);
 			soundFiles.playSound("menuSelect", sfxVolume);
 			break;
 		}
 
-		case Keys.NUMPAD_0:  {
-			if (chP1Ready.isVisible() && chP2Ready.isVisible()) {
-				btnPlayGame.setText("Ready");
-				btnPlayGame.setDisabled(false);
-			}
+		case Keys.NUMPAD_0: {			
 			chP2Ready.setVisible(true);
 			soundFiles.playSound("menuSelect", sfxVolume);
 			break;
 		}
 
-		case Keys.ENTER:  {
-			charSelectionMusic.stop();
-			game.setScreen(new GameScreen(game));
-			soundFiles.playSound("menuSelect", sfxVolume);
-			hide();
+		case Keys.ENTER: {
+			if (chP1Ready.isVisible() && chP2Ready.isVisible()) {
+				charSelectionMusic.stop();
+				game.setScreen(new GameScreen(game));
+				soundFiles.playSound("menuSelect", sfxVolume);
+				hide();
+			}
 			break;
 		}
 
-		case Keys.ESCAPE:  {
+		case Keys.ESCAPE: {
 			chP1Ready.setVisible(false);
 			chP2Ready.setVisible(false);
 			break;
 		}
-		
-		case Keys.BACKSPACE:  {
+
+		case Keys.BACKSPACE: {
 			soundFiles.playSound("menuBack", sfxVolume);
 			charSelectionMusic.stop();
 			game.setScreen(new MenuScreen(game));
@@ -464,43 +411,43 @@ public class CharacterSelectScreen implements Screen {
 	}
 
 	// method to change the texture based on character index
-	private Sprite changeCharacter(int chIndex) {
-		Sprite chTexture = null;
+	private Texture changeCharacter(int chIndex) {
+		Texture chTexture = null;
 		switch (chIndex) {
 		case 0: {
-			chTexture = new Sprite(TextureFiles.getCharacterTexture("ichigo"));
+			chTexture = TextureFiles.getCharacterTexture("ichigo");
 			break;
 		}
 		case 1: {
-			chTexture = new Sprite(TextureFiles.getCharacterTexture("byakuya"));
+			chTexture = TextureFiles.getCharacterTexture("byakuya");
 			break;
 		}
 		case 2: {
-			chTexture = new Sprite(TextureFiles.getCharacterTexture("ikkaku"));
+			chTexture = TextureFiles.getCharacterTexture("ikkaku");
 			break;
 		}
 		case 3: {
-			chTexture = new Sprite(TextureFiles.getCharacterTexture("ishida"));
+			chTexture = TextureFiles.getCharacterTexture("ishida");
 			break;
 		}
 		case 4: {
-			chTexture = new Sprite(TextureFiles.getCharacterTexture("kenpachi"));
+			chTexture = TextureFiles.getCharacterTexture("kenpachi");
 			break;
 		}
 		case 5: {
-			chTexture = new Sprite(TextureFiles.getCharacterTexture("urahara"));
+			chTexture = TextureFiles.getCharacterTexture("urahara");
 			break;
 		}
 		case 6: {
-			chTexture = new Sprite(TextureFiles.getCharacterTexture("komamura"));
+			chTexture = TextureFiles.getCharacterTexture("komamura");
 			break;
 		}
 		case 7: {
-			chTexture = new Sprite(TextureFiles.getCharacterTexture("mayuri"));
+			chTexture = TextureFiles.getCharacterTexture("mayuri");
 			break;
 		}
 		case 8: {
-			chTexture = new Sprite(TextureFiles.getCharacterTexture("renji"));
+			chTexture = TextureFiles.getCharacterTexture("renji");
 			break;
 		}
 		}
